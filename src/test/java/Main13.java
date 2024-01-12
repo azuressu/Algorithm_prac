da29b2024e92c238017e5a70f66bc11ce21db7eb
azuressu <martina0130@naver.com>
2024-01-12 21:34:02 +0900

    :memo: [심화1] 별찍기7

---
+++ src/test/java/Main13.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+
+        String str = " ";
+        int N = sc.nextInt();
+
+        for (int i=1; i<=N; i++) {
+            if (i != 5) sb.append(str.repeat(5-i)).append("*".repeat(2*i-1)).append("\n");
+            else sb.append("*".repeat(2*i-1)).append("\n");
+        }
+        for (int j=N-2; j>=0; j--) {
+            sb.append(str.repeat(4-j)).append("*".repeat(2*j+1));
+            if (j != 0) sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
