e35470d85fc8d98c92c9a62a97a3265831ba33d5
azuressu <martina0130@naver.com>
2024-01-19 19:29:32 +0900

    :memo: 모스부호

---
+++ src/test/java/p/Main2.java
@@ -0,0 +1,56 @@
+package p;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        Map<String, String> morse = new HashMap<>();
+        inputMorse(morse);
+
+        while (st.hasMoreTokens()) {
+            String s = st.nextToken();
+            sb.append(morse.get(s));
+        }
+
+        System.out.println(sb);
+    }
+
+
+    private static void inputMorse(Map<String, String> morse) {
+        morse.put(".-", "a");
+        morse.put("-...", "b");
+        morse.put("-.-.", "c");
+        morse.put("-..", "d");
+        morse.put(".", "e");
+        morse.put("..-.", "f");
+        morse.put("--.", "g");
+        morse.put("....", "h");
+        morse.put("..", "i");
+        morse.put(".---", "j");
+        morse.put("-.-", "k");
+        morse.put(".-..", "l");
+        morse.put("--", "m");
+        morse.put("-.", "n");
+        morse.put("---", "o");
+        morse.put(".--.", "p");
+        morse.put("--.-", "q");
+        morse.put(".-.", "r");
+        morse.put("...", "s");
+        morse.put("-", "t");
+        morse.put("..-", "u");
+        morse.put("...-", "v");
+        morse.put(".--", "w");
+        morse.put("-..-", "x");
+        morse.put("-.--", "y");
+        morse.put("--..", "z");
+    }
+
+}
